0184c2059f0e20c5a6ae5ef94fdd148852de7d14
JCKH <55143530+jckh-dev@users.noreply.github.com>
2021-05-30 15:59:01 +1000

    working

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,6 @@
 module.exports = {
     client: 'mysql2', connection: {
         host: '127.0.0.1', database: 'world',
-        user: 'root', password: 'XXXXX'
+        user: 'root', password: 'Dcba1234'
     }
 }
