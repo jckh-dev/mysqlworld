19e9b78692432da5ffe757672558bc7c80645b41
JCKH <55143530+jckh-dev@users.noreply.github.com>
2021-05-30 15:40:08 +1000

    update

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'The world database API' });
 });
 
 router.get('/api', function (req, res, next) {
   res.render('index', { title: 'Lots of routes available' });
+});
+
+router.get("/api/city", function (req, res, next) {
+  req.db.from('city')
+    .select("name", "district")
+    .then((rows) => { res.json({ "Error": false, "Message": "Success", "City": rows }) })
+    .catch((err) => {
+      console.log(err); res.json({ "Error": true, "Message": "Error in  MySQL query" })
+    })
 });
 
 module.exports = router;
